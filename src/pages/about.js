32725669a8505421288647b20fb71e5ae32b3dec
Robbie Pallas <robbie.pallas@piksel.com>
2019-10-15 01:22:45 +0100

    updates CV and details

--- src/pages/about.js
+++ src/pages/about.js
@@ -169,7 +169,7 @@ export default () => (
         </Row>
         <Row>
           <Col>
-            <h5>Computing Degree</h5>
+            <h5>Bachelor's Degree, Computing</h5>
           </Col>
         </Row>
         <Row>
